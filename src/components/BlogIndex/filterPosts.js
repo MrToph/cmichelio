2bfedcae4d0743435f08ed00c38a23913e059e6b
Christoph Michel <christoph.michel.1992@gmail.com>
2017-12-17 23:33:59 +0100

    Make post categories filter case-insensitive

--- src/components/BlogIndex/filterPosts.js
+++ src/components/BlogIndex/filterPosts.js
@@ -1,10 +1,12 @@
 import get from 'lodash/get'
 
 export default function filterPosts(posts, category) {
-    if(!category) return posts
+  if (!category) return posts
 
-    return posts.filter(({ node }) => {
-        const categories = get(node, 'frontmatter.categories', [])
-        return categories.some(postCategory => postCategory === category)
-    })
+  return posts.filter(({ node }) => {
+    const categories = get(node, 'frontmatter.categories', [])
+    return categories.some(
+      postCategory => postCategory.toLowerCase() === category.toLowerCase()
+    )
+  })
 }
