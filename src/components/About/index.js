139e1cce3b5c06ad8b56bc60b05203d4801b3051
Christoph Michel <christoph.michel.1992@gmail.com>
2017-12-17 14:56:19 +0100

    Change About me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -11,15 +11,15 @@ export default class AboutContent extends React.Component {
         <p>
           I {`'`}m <strong>Christoph Michel</strong>. I hold a bachelor{`'`}s
           degree in both <em>Math</em> and <em>Computer Science</em> and
-          finished my master studies in <em>Cryptography</em>
-          this year.
+          finished my master studies in <em>(Theoretical) Cryptography</em> this
+          year.
         </p>
         <p>
-          I have a broad interest in many things, always learning, and on my
-          quest to mastery. Right now, I{`'`}m working as a full stack software
-          engineer in the <em>JavaScript / Node.js</em> eco-system. I dedicate
-          most of my free time to the <em>gym</em>, <em>eating</em> a lot,{' '}
-          <em>open-source</em>, and innovative <em>blockchain</em> projects.
+          I have a broad interest in many things, and am always learning. Right
+          now, I{`'`}m working as a full stack software engineer in the{' '}
+          <em>JavaScript / Node.js</em> eco-system. I dedicate most of my free
+          time to the <em>gym</em>, <em>eating</em> a lot, <em>open-source</em>,{' '}
+          <em>teaching</em>, and innovative <em>blockchain</em> projects.
         </p>
         <ContactForm />
       </section>
