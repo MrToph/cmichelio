1fdebae4b4c8f4b5375068b4998daf83baf02f8f
Christoph Michel <christoph.michel.1992@gmail.com>
2018-09-25 20:53:17 +0200

    Add 'what others say' section to about me. Fix broken link

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,20 +9,35 @@ export default class AboutContent extends React.Component {
         <PageTitle>About</PageTitle>
         <h2>Welcome to cmichel.io</h2>
         <p>
-          I {`'`}m <strong>Christoph Michel</strong>. I hold a bachelor{`'`}s
-          degree in both <em>Math</em> and <em>Computer Science</em> and
-          finished my master studies in <em>(Theoretical) Cryptography</em> last
-          year.
+          I {`'`}m <strong>Christoph Michel</strong>. I hold bachelor{`'`}s
+          degree and master's degrees in both <em>Math</em> and <em>Computer Science</em> with a focus on <em>(Theoretical) Cryptography</em>.
         </p>
         <p>
-          I have a broad interest in many things, and am always learning. Right
+          I have a broad interest in many things, and am always curious. Right
           now, I{`'`}m working as a full stack software engineer in the{' '}
           <em>JavaScript / Node.js</em> eco-system. I dedicate most of my free
           time to the <em>gym</em>, <em>eating</em> a lot, <em>open-source</em>,{' '}
           <em>teaching</em>, and innovative <em>blockchain</em> projects.
         </p>
+        <h3>What other people say about me</h3>
+        <ul>
+          <li>
+            Mark Erikson, maintainer of Redux, in his <a href="https://www.reddit.com/r/reactjs/comments/5t8loz/what_are_your_top_reactreact_native_blogs_that/" rel="nofollow">high quality blogs</a> reddit post:
+            <blockquote>Christoph Michel has written a number of good articles about React, React Native, and Redux</blockquote>
+          </li>
+          <li>
+            Considered one of the <a href="https://ideamotive.co/blog/best-react-native-experts-blogs/" rel="nofollow">17 React Native experts and Blogs To follow in 2018</a>.
+            <blockquote>Christoph has written a number of good articles about React, React Native, and Redux. Although he doesn’t exclusively write about mobile app development, his blog is still worth visiting.</blockquote>
+          </li>
+          <li>
+            Receiver of <a href="https://www.fdsi.org/index.php?id=7">2016's "Günter Hotz medal"</a> for outstanding academical graduate studies.
+          </li>
+        </ul>
         <ContactForm />
       </section>
     )
   }
 }
+
+// * https://www.reddit.com/r/reactjs/comments/5t8loz/what_are_your_top_reactreact_native_blogs_that/
+// * Expert React Native
--- src/components/Portfolio/IsomorphicButton.jsx
+++ src/components/Portfolio/IsomorphicButton.jsx
@@ -6,44 +6,44 @@ import { primaryColor, primaryColorLight } from '../../styling'
 
 const buttonStyles = css(
   {
-    position: 'relative',
-    border: 'none',
-    fontFamily: 'inherit',
-    fontSize: '14px',
-    cursor: 'pointer',
-    padding: '1px 15px',
-    display: 'inline-block',
-    margin: '15px 0',
-    textTransform: 'uppercase',
+    position: `relative`,
+    border: `none`,
+    fontFamily: `inherit`,
+    fontSize: `14px`,
+    cursor: `pointer`,
+    padding: `1px 15px`,
+    display: `inline-block`,
+    margin: `15px 0`,
+    textTransform: `uppercase`,
     // letterSpacing: '1px',
-    fontWeight: '700',
-    outline: 'none',
-    textDecoration: 'none',
+    fontWeight: `700`,
+    outline: `none`,
+    textDecoration: `none`,
     background: `${primaryColorLight}`,
-    color: '#fff',
+    color: `#fff`,
     // background: '#f0f0f0',
     // color: '#3a3a3a',
 
     boxShadow: `0 3px ${primaryColor}`,
     top: 0,
-    transition: 'none',
-    borderRadius: '40px',
+    transition: `none`,
+    borderRadius: `40px`,
 
     '&:hover': {
       boxShadow: `0 2px ${primaryColor}`,
-      top: '1px',
+      top: `1px`,
     },
 
     '&:active': {
       boxShadow: `0 0 ${primaryColor}`,
-      top: '4px',
+      top: `4px`,
     },
   },
   after({
-    content: '',
-    position: 'absolute',
-    zIndex: '-1',
-    transition: 'all 0.3s',
+    content: ``,
+    position: `absolute`,
+    zIndex: `-1`,
+    transition: `all 0.3s`,
   })
 )
 
@@ -53,11 +53,18 @@ export default class IsomorphicButton extends React.Component {
     children: PropTypes.node.isRequired,
   }
   render() {
-    return (
-      <Link to={this.props.url} className="no-style" {...buttonStyles}>
-        {this.props.children}
-      </Link>
-    )
+    const internal = /^\/(?!\/)/.test(this.props.url)
+
+    if(internal) {
+
+      return (
+        <Link to={this.props.url} >
+          
+        </Link>
+      )
+    }
+
+    return <a href={this.props.url} className="no-style" rel="noopener" {...buttonStyles}>{this.props.children}</a>
   }
 }
 
