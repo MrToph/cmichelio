2f5381141cf23b4a81e58979708757c16aaaa405
MrToph <christoph.michel.1992@gmail.com>
2019-07-15 19:06:01 +0200

    Remove favicon background and use different sizes

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -31,6 +31,13 @@ export default function MainTemplate(props) {
     <React.Fragment>
       <Helmet defaultTitle={siteMetadata.title}>
         <html lang="en" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
         <link rel="canonical" href={siteMetadata.siteUrl} />
         <meta name="description" content={siteMetadata.description} />
         <meta name="keywords" content={siteMetadata.keywords} />
